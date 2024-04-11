7a3174261b81d3f0a7257acb3ebbca00152890ec
Rayyze <chppleo@gmail.com>
2024-04-11 21:05:31 +0200

    gradecombination a 85%

--- src/welcome/ia/Strat64.java
+++ src/welcome/ia/Strat64.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import welcome.Jeu;
 import welcome.Joueur;
+import welcome.Lotissement;
 import welcome.Travaux;
 import welcome.Ville;
 import welcome.Maison;
@@ -15,13 +16,14 @@ import welcome.utils.RandomSingleton;
 
 public class Strat64 extends Strat{
 
-    public Map<String, Double> weigths = new HashMap<String, Double>();
+    public Map<String, Double> weights = new HashMap<String, Double>();
     /*
      * park1
      * park2
      * park3
      * pool
      * interim
+     * interimneeded
      * bisneeded
      * bisnotneeded
      * lot1
@@ -38,6 +40,18 @@ public class Strat64 extends Strat{
     
     public Strat64(){
 
+    }
+
+    public Map<String, Double> getWeights() {
+        return weights;
+    }
+
+    public void setWeights(Map<String, Double> inputWeights) { //TODO mettre en private avant d'envoyer
+        for (Map.Entry<String, Double> entry : inputWeights.entrySet()) {
+            String key = entry.getKey();
+            Double value = entry.getValue();
+            weights.put(key, value);
+        }
     }
     
     @Override
@@ -143,7 +157,6 @@ public class Strat64 extends Strat{
             while(min>=0  && (joueur.ville.rues[i].maisons[min].numero==-1 || joueur.ville.rues[i].maisons[min].numero > numero))
                 min--; // on décrement le min tant qu'on a pas trouvé un numéro <=
             if(min<0 || joueur.ville.rues[i].maisons[min].numero!=numero){
-
                 min++;// On part de la case suivante
                 while(min < joueur.ville.rues[i].taille && joueur.ville.rues[i].maisons[min].numero == -1){
                     possibilite.add((Integer)(min+ 100*i)); // on construit les possibilités tant qu'on a des cases vides
@@ -154,18 +167,60 @@ public class Strat64 extends Strat{
         return possibilite;
     }
 
+    //Construction des possibilités de placement des numéros bis
+    private ArrayList<Integer> constuireBis(Joueur joueur){
+        ArrayList<Integer> possibilite= new ArrayList<Integer>();
+        possibilite.add(1000);
+        for(int i=0; i<3; i++){ //on parcours les rues
+            for(int j=0; j<joueur.ville.rues[i].taille; j++){ //on parcours les maisons
+                if(joueur.ville.rues[i].maisons[j].estVide()){ //Pour chaque maison vide
+                    if(j>0 && !joueur.ville.rues[i].maisons[j-1].estVide() && !joueur.ville.barrieres[i][j]){ //On check le numero à gauche
+                        possibilite.add(-1 * i * 100 - j);
+                    }
+                    if(j<joueur.ville.rues[i].taille-1 && !joueur.ville.rues[i].maisons[j+1].estVide() && !joueur.ville.barrieres[i][j+1]){//On check le numero à droite
+                        possibilite.add(i * 100 + j);
+                    }
+                }
+            }
+        }
+        return possibilite;
+    }
+
+    //Construction des possibilités pour les choix de barrières
+    private ArrayList<Integer> construireChoixPlacementBarriere(Joueur joueur) {
+        Lotissement l;
+        //Les possibilités seront ajoutés dans une liste
+        //Une possibilité sera représenté par un entier ayant la valeur suivante: 100*n°rue + position dans la rue
+        //Création de la liste
+        ArrayList<Integer> possibilite= new ArrayList<Integer>();
+        possibilite.add(0);
+        //On parcours les lotissements
+        for(int i=0; i<joueur.ville.lotissements.size(); i++){
+            l=joueur.ville.lotissements.get(i);
+            if(l.dispo){//Si le lotissement est dispo (pas encore utilisé pour la validation d'un objectif)
+                for(int j=l.debut+1; j<l.fin; j++){ //on ajoute aux possibilités les barrières entre les 2 barrières du lotissements   
+                    if(joueur.ville.rues[l.rue.numero-1].maisons[j-1].numero <0 || joueur.ville.rues[l.rue.numero-1].maisons[j-1].numero != joueur.ville.rues[l.rue.numero-1].maisons[j].numero){
+                        possibilite.add((Integer)((l.rue.numero-1) * 100 + j));
+                    }
+                }
+            }
+        }
+        return possibilite;
+    }
+
     private Map<String, Integer> gradeCombination(Jeu j, int joueur) {
         String key = "";
-        int score;
+
+        ArrayList<ArrayList<Integer>> numerosInterim = new ArrayList<ArrayList<Integer>>();
 
         int numero;
         String action;
         ArrayList<Integer> possibilities;
 
-        //decisionsScoreMap
-
         for (int i=0; i<3; i++) {
             key = Integer.toString(i);
+
+            numerosInterim.add(new ArrayList<Integer>());
 
             //On récupère les combinaisons
             numero = ((Travaux) j.numeros[i].top()).getNumero();
@@ -178,40 +233,68 @@ public class Strat64 extends Strat{
                 switch (action) {
                     case "Fabricant de piscine":
                         if (isOnPool(j, joueur, possibilities.get(k))) {
-                            decisionsScoreMap.put(key + ",pool", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weigths.get("pool"));
+                            decisionsScoreMap.put(key + ",pool", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weights.get("pool"));
                         } else {
                             decisionsScoreMap.put(key + ",nopool", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)));
                         }
                         break;
 
                     case "Agence d'intérim":
-                        //TODO
+                        if(isInterimNeeded(j, joueur, possibilities.get(k))) {
+                            decisionsScoreMap.put(key + ",0" + "interim", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weights.get("interimneeded"));
+                        } else {
+                            decisionsScoreMap.put(key + ",0" + "interim", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weights.get("interim"));
+                        }
+                        for (int l = -2; l<=2; l++) {
+                            if (numero+l>=0) {
+                                numerosInterim.get(i).add(numero+l);
+                            }
+                        }
                         break;
 
                     case "Numéro Bis\t":
-                        //TODO
+                        //TODO parcourir les possibilités de placement de bis
                         break;
                     
                     case "Paysagiste\t":
                         if (isParkFull(j, joueur, possibilities.get(k))) {
                             decisionsScoreMap.put(key + ",nopark", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)));
                         } else {
-                            decisionsScoreMap.put(key + ",park", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weigths.get("park" + Integer.toString(possibilities.get(k)/100 + 1)));
+                            decisionsScoreMap.put(key + ",park", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weights.get("park" + Integer.toString(possibilities.get(k)/100 + 1)));
                         }
                         break;
 
                     case "Agent Immobilier":
-                        for(int l=1; l<7; l++) { //TODO ajouter un if pour savoir si l'investissemenjt est plein
-                            decisionsScoreMap.put(key + "," + Integer.toString(l), distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weigths.get("lot" + Integer.toString(l)));
+                        for(int l=1; l<7; l++) {
+                            if(j.joueurs[joueur].ville.avancementPrixLotissement[l-1]==j.joueurs[joueur].ville.maxAvancement[l-1]) {
+                                decisionsScoreMap.put(key + "," + Integer.toString(l) + "full", distanceToIdealPlace(j, joueur, numero, possibilities.get(k)));
+                            } else {
+                                decisionsScoreMap.put(key + "," + Integer.toString(l), distanceToIdealPlace(j, joueur, numero, possibilities.get(k)) + weights.get("lot" + Integer.toString(l)));
+                            }
                         }
                         break;
 
                     case "Géomètre\t":
-                        //TODO
+                        //TODO parcourir les possibilités de placement de barrières
                         break;
 
                     default: 
                         break;
+                }
+            }
+        }
+
+        for (int i=0; i<3; i++) {
+            key = Integer.toString(i);
+            for (int k=0; k<numerosInterim.get(i).size(); k++){
+                ArrayList<Integer> interimPossibilities = construirePossibilite(numerosInterim.get(i).get(k), j.joueurs[joueur]);
+
+                for (int l=0; l<interimPossibilities.size(); l++) {
+                    if(isInterimNeeded(j, joueur, interimPossibilities.get(l))) {
+                        decisionsScoreMap.put(key + "," + Integer.toString(l) + "interim", distanceToIdealPlace(j, joueur, numerosInterim.get(i).get(k), interimPossibilities.get(l)) + weights.get("interimneeded"));
+                    } else {
+                        decisionsScoreMap.put(key + "," + Integer.toString(l) + "interim", distanceToIdealPlace(j, joueur, numerosInterim.get(i).get(k), interimPossibilities.get(l)) + weights.get("interim"));
+                    }
                 }
             }
         }
@@ -278,7 +361,7 @@ public class Strat64 extends Strat{
         return (ville.nbParcs[indexRue]==ville.maxParcs[indexRue]);
     }
 
-    private boolean isFillable(Jeu j, int joueur, int place) {
+    private boolean isBisNeeded(Jeu j, int joueur, int place) {
         int numRue = place/100;
         Rue rue = j.joueurs[joueur].ville.rues[numRue];
         Maison[] maisons = rue.maisons;
@@ -299,6 +382,32 @@ public class Strat64 extends Strat{
         }
 
         if (count+1<=sup-inf) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isInterimNeeded(Jeu j, int joueur, int place) {
+        int numRue = place/100;
+        Rue rue = j.joueurs[joueur].ville.rues[numRue];
+        Maison[] maisons = rue.maisons;
+
+        int indFirst = -1;
+        int indLast = -1;
+
+        for (int i=0; i<rue.taille; i++) {
+            if (!maisons[i].estVide()) {
+                if (indFirst==-1) {
+                    indFirst = i;
+                }
+                if (indLast<i) {
+                    indLast = 1;
+                }
+            }
+        }
+
+        if ((place%100<indFirst && maisons[indFirst].numero==1) || (place%100>indLast && (maisons[indLast].numero==15 || maisons[indLast].numero==16))) {
             return true;
         } else {
             return false;
